8e18dca6f154f1a0ba14adbcfc797d64015418df
DarBaala <joker.milan1997gmail.com>
2022-04-09 21:46:53 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-/// Товарищ Юсуп, данная функция отвечает за отклик кнопки "Выбрать клуб"
+"use strict";
+
+/// (2 пункт ТЗ) Товарищ Юсуп, данная функция отвечает за отклик кнопки "Выбрать клуб"
 const chooseClub = () => {
   const clubList = document.getElementById("club");
   const bodyClub = document.getElementById("body-club");
-  document.body.addEventListener("click", (event) => {
-    let target = event.target;
-    if (target === clubList) {
+  clubList.addEventListener("click", () => {
+    document.body.addEventListener("click", (event) => {
       bodyClub.style.display = "block";
-    } else if (target !== clubList) {
-      bodyClub.style.display = "none";
-    }
+      let target = event.target;
+      if (target === clubList) {
+        bodyClub.style.display = "block";
+      } else if (target !== clubList) {
+        bodyClub.style.display = "none";
+      }
+    });
   });
 };
 chooseClub();
+/// (3 пункт ТЗ) Открывает модальное окно free_visit_form
+const popup = () => {
+  const openPopup = document.getElementById("free_visit_form");
+  const popupActive = document.querySelector(".popup-free_visit_form");
+  const overlayClose = document.querySelector(".form-wrapper");
+  openPopup.addEventListener("click", () => {
+    document.body.addEventListener("click", (event) => {
+      popupActive.style.display = "block";
+      let target = event.target;
+      if (target === openPopup) {
+        popupActive.style.display = "block";
+      }
+      if (target !== overlayClose) {
+        console.log(555);
+      }
+    });
+  });
+};
+popup();
+//free_visit_form
+const formWrapper = document.querySelector(".form-content");
