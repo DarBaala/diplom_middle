6dbf972198a1da38abfbd4507e14c8ff610dc13f
DarBaala <joker.milan1997gmail.com>
2022-04-11 01:38:32 +0300

    added popup open function

--- src/index.js
+++ src/index.js
@@ -2,38 +2,53 @@
 
 /// (2 пункт ТЗ) Товарищ Юсуп, данная функция отвечает за отклик кнопки "Выбрать клуб"
 const chooseClub = () => {
-  const clubList = document.getElementById("club");
-  const bodyClub = document.getElementById("body-club");
-  clubList.addEventListener("click", () => {
-    document.body.addEventListener("click", (event) => {
+  const openClub = (event) => {
+    let target = event.target;
+    const clubList = document.getElementById("club"),
+      bodyClub = document.getElementById("body-club");
+    if (target === clubList) {
       bodyClub.style.display = "block";
-      let target = event.target;
-      if (target === clubList) {
-        bodyClub.style.display = "block";
-      } else if (target !== clubList) {
-        bodyClub.style.display = "none";
-      }
-    });
-  });
+    } else if (target !== bodyClub) {
+      bodyClub.style.display = "none";
+    }
+  };
+  document.body.addEventListener("click", openClub);
 };
 chooseClub();
+
 /// (3 пункт ТЗ) Открывает модальное окно free_visit_form
-const popup = () => {
-  const openPopup = document.getElementById("free_visit_form");
-  const popupActive = document.querySelector(".popup-free_visit_form");
-  const overlayClose = document.querySelector(".form-wrapper");
-  openPopup.addEventListener("click", () => {
-    document.body.addEventListener("click", (event) => {
-      popupActive.style.display = "block";
-      let target = event.target;
-      if (target === openPopup) {
-        popupActive.style.display = "block";
-      }
-      if (target !== overlayClose) {
-        console.log(555);
-      }
-    });
-  });
+const popupVisit = () => {
+  const openVisit = (event) => {
+    let target = event.target;
+    const freeVisit = document.getElementById("free_visit_form"),
+      closeIcon = document.getElementById("visit-close"),
+      visitOverlay = document.getElementById("visit-overlay");
+    if (target.closest(".open-popup")) {
+      freeVisit.style.display = "flex";
+    } else if (target === closeIcon || target === visitOverlay) {
+      freeVisit.style.display = "none";
+    }
+  };
+  document.body.addEventListener("click", openVisit);
 };
-popup();
-//free_visit_form
+popupVisit();
+
+/// (4 пункт ТЗ) Открывается модальное окно callback_form
+const callbackForm = () => {
+  const openCallback = (event) => {
+    let target = event.target;
+    const buttonCallback = document.querySelector(".callback-btn"),
+      overlayCallback = document.getElementById("overlay-callback"),
+      callback = document.getElementById("callback_form"),
+      closeCallback = document.getElementById("callback-close");
+    if (target === buttonCallback) {
+      callback.style.display = "flex";
+    } else if (target === overlayCallback || target === closeCallback) {
+      callback.style.display = "none";
+    }
+  };
+  document.body.addEventListener("click", openCallback);
+};
+callbackForm();
+
+/// (5 пункт ТЗ) Все формы на странице и в модальных окнах должны отправляться посредством ajax
