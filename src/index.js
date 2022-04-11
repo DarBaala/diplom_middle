5bdafae039d9e0df19768ec8a9d604edbabf0f1d
DarBaala <joker.milan1997gmail.com>
2022-04-11 20:56:17 +0300

    added sendForm

--- src/index.js
+++ src/index.js
@@ -52,3 +52,37 @@ const callbackForm = () => {
 callbackForm();
 
 /// (5 пункт ТЗ) Все формы на странице и в модальных окнах должны отправляться посредством ajax
+const sendForm = (formName, color = "color: white;") => {
+  const form = document.getElementById(formName),
+    errorMessage = "Что-то пошло не так :(",
+    loadMessage = "Загрузка...",
+    successMessage = "Спасибо! Мы скоро с Вами свяжемся!";
+  let statusMessage = document.createElement("div");
+  statusMessage.style.cssText = `${color} padding-top: 10px; font-size: 14px;`;
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+      statusMessage.textContent = loadMessage;
+      if (request.readyState != 4) {
+        return;
+      }
+      if (request.status === 200) {
+        statusMessage.textContent = successMessage;
+        form.reset();
+      } else {
+        statusMessage.textContent = errorMessage;
+      }
+    });
+    request.open("POST", "./server.php");
+    request.setRequestHeader("Content-Type", "multipart/form-data");
+    const formData = new FormData(form);
+    request.send(formData);
+  });
+};
+sendForm("form1");
+sendForm("form2");
+sendForm("banner-form");
+sendForm("card_order", "color: black;");
+sendForm("footer_form");
